fc9901f202560388b39d7d92245efc355b89350d
AnneGuby <anne.gubynath@gmail.com>
2024-07-01 16:58:25 -0400

    Add files via upload

---
+++ Data/Entities.cs
@@ -0,0 +1,16 @@
+﻿using EmployeesAPI.Domains;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EmployeesAPI.Data
+{
+    public class Entities :  DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeType> EmployeeTypes { get; set; }
+    }
+}
